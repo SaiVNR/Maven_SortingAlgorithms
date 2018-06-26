aa9ec9cf1ed0661751aa97ff8c9d5ca44b84ffb3
SAI TEJA SUGGULA <32015565+SaiVNR@users.noreply.github.com>
2018-06-26 16:17:48 +0530

    Update App.java

--- Teja_Maven/BubbleSort_Maven/src/main/java/com/teja/BubbleSort_Maven/App.java
+++ Teja_Maven/BubbleSort_Maven/src/main/java/com/teja/BubbleSort_Maven/App.java
@@ -1,14 +1,11 @@
 package com.teja.BubbleSort_Maven;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
     }
     
     public int[] bubbleSort(int arr[])
